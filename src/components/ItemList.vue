a692f687553a4641ead729498954c3f6f5bf06ce
WistreHosshii <sao_heath6147.wistre@icloud.com>
2019-05-12 00:19:01 +0900

    todolistの基幹部分の実装

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>ItemList</div>
-    <div v-for="item in items" :key="item.name">
+    <div v-for="item in items" :key="item.id" id="list">
       <div class="item" :class="{over500: item.price>=500}">
         <div class="name">名前: {{ item.name }}</div>
         <div class="price">{{ item.price }} 円</div>
@@ -23,16 +23,18 @@
 </template>
 
 <script>
+let itemeid = 5
 export default {
   name: "ItemList",
+  
   data(){
       return {
         items:[
-              {name:'たまご', price:'100'},
-              {name:'りんご', price:'160'},
-              {name:'アイス', price:'99'},
-              {name:'パン', price:'156'},
-              {name:'うまい棒', price:'10'}
+              {name:'たまご', price:'100', id: 1},
+              {name:'りんご', price:'160', id: 2},
+              {name:'アイス', price:'99', id: 3},
+              {name:'パン', price:'156',id: 4},
+              {name:'うまい棒', price:'10', id: 5}
           ],
         newItemName: "",
         newItemPrice: 0
@@ -41,7 +43,8 @@ export default {
    methods: {
     addItem() {
         if(this.newItemName!==""){
-            this.items.push({ name: this.newItemName, price: this.newItemPrice });
+          itemeid++
+            this.items.push({ name: this.newItemName, price: this.newItemPrice ,id: itemeid})
             this.newItemName = "",
             this.newItemPrice  = 0
         }
